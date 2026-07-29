d32e9c0a42ccd9a8d03a30ebdcc0a25725e2ee84
SangDonPark <sdp14222@gmail.com>
2026-07-30 02:11:47 +0900

    Modified : ftrace - func_stack_trace 관련 추가
    1. func_stack_trace 사용 시에는 current_tracer를 function으로 할 것

--- device_tree_modified/set_ftrace.sh
+++ device_tree_modified/set_ftrace.sh
@@ -4,13 +4,17 @@ echo 0 > /sys/kernel/debug/tracing/tracing_on
 sleep 1
 echo "tracing_off"
 
-echo function_graph > /sys/kernel/debug/tracing/current_tracer
-#echo function > /sys/kernel/debug/tracing/current_tracer
+#echo function_graph > /sys/kernel/debug/tracing/current_tracer
+echo function > /sys/kernel/debug/tracing/current_tracer
 sleep 1
 echo "set function_graph tracer"
 
 echo gpio_demo_isr > /sys/kernel/debug/tracing/set_ftrace_filter
 echo "filter : gpio_demo_isr"
+sleep 1
+
+echo 1 > /sys/kernel/debug/tracing/options/func_stack_trace
+echo "function stack trace enabled"
 sleep 1
 
 echo 1 > /sys/kernel/debug/tracing/tracing_on
