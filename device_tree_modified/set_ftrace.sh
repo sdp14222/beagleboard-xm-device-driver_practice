1a2315286a8d17a19fdfd59dd0a01819072e4e16
SangDonPark <sdp14222@gmail.com>
2026-07-30 02:22:25 +0900

    Added : ftrace - function_stack_trace 관련 설정 추가
    1. 관련 로그 파일 추가

--- device_tree_modified/set_ftrace.sh
+++ device_tree_modified/set_ftrace.sh
@@ -4,14 +4,23 @@ echo 0 > /sys/kernel/debug/tracing/tracing_on
 sleep 1
 echo "tracing_off"
 
-echo function_graph > /sys/kernel/debug/tracing/current_tracer
-#echo function > /sys/kernel/debug/tracing/current_tracer
+# 둘 중 선택 (function_graph, function)
+#echo function_graph > /sys/kernel/debug/tracing/current_tracer
+#echo "set function_graph tracer"
+echo function > /sys/kernel/debug/tracing/current_tracer
+echo "set function tracer"
 sleep 1
-echo "set function_graph tracer"
 
-echo gpio_demo_isr > /sys/kernel/debug/tracing/set_ftrace_filter
-echo "filter : gpio_demo_isr"
+#echo gpio_demo_isr > /sys/kernel/debug/tracing/set_ftrace_filter
+echo gpio_demo_debounce_work > /sys/kernel/debug/tracing/set_ftrace_filter
+echo "filter : gpio_demo_debounce_work"
 sleep 1
+
+# func_stack_trace를 사용할 경우 function_graph가 제대로 동작하지 않는 것 같음
+echo 1 > /sys/kernel/debug/tracing/options/func_stack_trace
+echo "function stack trace enabled"
+sleep 1
+
 
 echo 1 > /sys/kernel/debug/tracing/tracing_on
 sleep 1
