13a02aab2df79bd861b735b4e15cd9fb41c4c0f8
SangDonPark <sdp14222@gmail.com>
2026-07-30 23:36:50 +0900

    Added : set_ftrace for sched switch

---
+++ device_tree_modified/set_ftrace_sched_switch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+sleep 1
+echo "tracing_off"
+
+echo function > /sys/kernel/debug/tracing/current_tracer
+echo "current tracer : function"
+sleep 1
+
+echo gpio_demo_isr > /sys/kernel/debug/tracing/set_ftrace_filter
+echo "set_trace_filter : gpio_demo_isr"
+echo gpio_demo_debounce_work >> /sys/kernel/debug/tracing/set_ftrace_filter
+echo "set_trace_filter : gpio_demo_debounce_work"
+
+echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable
+echo "sched_switch enabled"
+echo > /sys/kernel/debug/tracing/trace
+sleep 1
+
+echo 1 > /sys/kernel/debug/tracing/tracing_on
+echo "tracing_on"
+
